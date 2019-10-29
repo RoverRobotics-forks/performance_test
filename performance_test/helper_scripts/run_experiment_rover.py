fd4114d5fce69adb6db837cc37a392528345a8f0
Dan Rose <rotu@users.noreply.github.com>
2019-10-29 17:48:00 -0500

    Update run_experiment_rover.py

--- performance_test/helper_scripts/run_experiment_rover.py
+++ performance_test/helper_scripts/run_experiment_rover.py
@@ -9,18 +9,28 @@ def run_experiment(
         rmw='',
         num_subs=10,
         num_pubs=10,
+        timeout=30,
 ):
     topic_args = ['--topic {}'.format(t) for t in topics]
     rate_args = ['--rate {}'.format(r) for r in rates]
+        
     new_env = dict(os.environ)
     new_env['RMW_IMPLEMENTATION'] = rmw
-
-    subprocess.run(['perf_test',
+    
+    try:
+      res = subprocess.run(['perf_test',
                     '--topic','topic',
                     '--rate','rate',
-                    '--num_subs'
+                    '--num_subs' str(num_subs),
+                   ], timeout=timeout+0.5, capture_output=True, env=new_env)
+      o= res.stdout
+     except TimeoutExpired as e:
+      o = e.stdout
+     rawdata = {
+             "kwargs": kwargs,
+             "stdout": o,
+     }
 
-                    ], env=new_env)
 
 def run_experiments():
     pass
